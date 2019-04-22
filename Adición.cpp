c13583a4ae9b6fe2516dab5fd2baa169eb38b7b6
jmdemj <37813969+jmdemj@users.noreply.github.com>
2019-04-22 10:55:35 -0300

    Add files via upload

---
+++ Adición.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout<<"Ingrese el primer valor a sumar"<<endl;
+    cin>>a;
+
+    cout<<"Ingrese el segundo valor a sumar"<<endl;
+    cin>>b;
+
+    cout<<"La suma del primer y segundo valor ingresados es: "<<a+b<<endl;
+
+    return 0;
+}
